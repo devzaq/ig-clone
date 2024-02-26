10ecee1a75c31fae76eaef59e9b925d20af7e655
devzaq <aajjha9@gmail.com>
2024-02-27 01:22:58 +0530

    Snapgram first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const SignupValidation = z.object({
+    name: z.string().min(2,{message: 'Too Short'}),
+    username: z.string().min(2).max(50),
+    email: z.string().email(),
+    password: z.string().min(8,{message: "Password must be at least 8 charachters."})
+  })
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z.string().min(8,{message: "Password must be at least 8 charachters."})
+  })
